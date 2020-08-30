61c10e9f611dcbedd55ee921ff35fd54ed080dd2
Muhammad Haris <mharisshaikh12@gmail.com>
2020-08-30 16:27:34 +0500

    Updates

---
+++ precache-manifest.63301d2f7a471d3246d8c1e0bdd8d1c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34e3616cf64020e0c364bd49ac798467",
+    "url": "/git@github.com:mharisshaikh12/Demo.git/index.html"
+  },
+  {
+    "revision": "3652305f42699dafe8b8",
+    "url": "/git@github.com:mharisshaikh12/Demo.git/static/css/main.852e8e2c.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/git@github.com:mharisshaikh12/Demo.git/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git@github.com:mharisshaikh12/Demo.git/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3652305f42699dafe8b8",
+    "url": "/git@github.com:mharisshaikh12/Demo.git/static/js/main.d93491ed.chunk.js"
+  },
+  {
+    "revision": "b8f373cf591a3ca037f7",
+    "url": "/git@github.com:mharisshaikh12/Demo.git/static/js/runtime-main.7c1d8a90.js"
+  }
+]);

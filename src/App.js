a2ba5c1f5395b4e36ea78373b863b68ee329afd8
Muhammad Haris <mharisshaikh12@gmail.com>
2020-08-30 14:29:35 +0500

    finalize project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  /**
+   * @constructor
+   *
+   */
+  constructor () {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+  }
+
+  /**
+   * @method handleChange arrow function which call on onChange event
+   * and set state of searchField also it bind this because of arrow so we
+   * don't need to bind this method into Consturctor with this keyword 
+   *
+   * @param {event} e event of the change
+   */
+  handleChange = e => {
+    this.setState( { searchField: e.target.value } )
+  };
+
+  componentDidMount() {
+    fetch( 'https://jsonplaceholder.typicode.com/users' )
+      .then( response => response.json() )
+      .then( users => this.setState( { monsters: users } ) );
+  }
+
+  /**
+   * @method render method of react module that render everytime 
+   * whenever any changes occuers in App component.
+   *
+   */
+  render() {
+
+    const { monsters, searchField } = this.state;
+    const filterdMonsters = monsters.filter( monster => {
+      return monster.name.toLowerCase().includes( searchField.toLowerCase() );
+    } );
+
+    return (
+      <div className="App">
+        <h1>Rolodex Monsters</h1>
+        <SearchBox
+          placeholder='Search monsters here ...'
+          handleChange={ this.handleChange }
+        />
+        <CardList monsters={ filterdMonsters } />
+      </div>
+    );
+  }
 }
-
 export default App;
